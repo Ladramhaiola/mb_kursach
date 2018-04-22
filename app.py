b09bc7d557cfb170a85b66eb3433dddc4fb149f3
Andriy <andriy380@gmail.com>
2018-04-22 19:16:04 +0300

    sign-in/sign-up

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
-from flask import Flask, json, redirect, render_template, request, session
+from flask import Flask, json, redirect, render_template, request, session, url_for
 from models import User, Song, Playlist
 from database import db_session, init_db
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.secret_key =  'total secret'
 
 @app.route('/')
 def index():
-	return render_template('login.html')
+	return render_template('signin.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	username = request.form['username']
-	password = request.form['password']
-	s = db_session.query(User).filter(User.username == username).first()
-	if not s:
-		u = User(username=username, password=password)
-		db_session.add(u)
-		db_session.commit()
-		session['user'] = {'username':username, 'logged_in': True}
-		return 'Success'
-	return 'Accoun already exists'
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		s = db_session.query(User).filter(User.username == username).first()
+		if not s:
+			u = User(username=username, password=password)
+			db_session.add(u)
+			db_session.commit()
+			session['user'] = {'username':username, 'logged_in': True}
+			return 'Success'
+		return 'Account already exists'
+	else:
+		return render_template('signup.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		s = db_session.query(User).filter(User.username == username).first()
+		if s:
+			if check_password_hash(s.password, password):
+				session['user'] = {'username':username, 'logged_in':True}
+				return 'logged_in'
+			else:
+				return 'wrong password'
+		return 'Account does not exist'
+	else:
+		return render_template('signin.html')
+
+def logout():
+	session.pop('user')
 
 @app.teardown_appcontext
 def shutdown_session(param):
