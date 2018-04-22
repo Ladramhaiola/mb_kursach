be5b4118347b0d6ae7d79e6e558f1cbff7266d66
Александра Кузьменко <aluminium_71@outlook.com>
2018-04-22 21:12:19 +0300

    add f

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import User, Playlist, Song
 
 engine = create_engine('mysql+pymysql://kursach:Lv1q9veCY-_f@den1.mysql2.gear.host:3306/kursach')
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
@@ -9,8 +10,18 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-	from models import User, Playlist, Song
+	
 	# Base.metadata.drop_all(bind=engine)
 	Base.metadata.create_all(bind=engine)
 	
 	db_session.commit()
+
+def add_Playlist(username, playlistname):
+	p = Playlist(name=playlistname)
+	db_session.add(p)
+	u = db_session.query(User).filter(User.username == username).first()
+	u.playlists.append(p)
+	db_session.commit()
+	
+
+	pass
